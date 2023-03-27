199d8d89bba3a93fb3688b06350dd464077ac9fc
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-03-27 08:11:11 +0000

    "finish"

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,68 @@
+class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
+  def new
+   @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
+    @book_new = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+      book = Book.find(params[:id])
+      unless book.user.id == current_user.id
+      redirect_to books_path
+      end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+  has_one_attached :profile_image
+
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
+
+   def get_image
+    if profile_image.attached?
+      profile_image
+    else
+      'no_image.jpg'
+    end
+
+     unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+     profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+   end
+end
